e36a3e6ffb9729846f26c354ba1eca18deb86e43
ranjanin <nvranjani@gmail.com>
2014-12-27 12:20:11 +0000

    migrations

--- db/migrate/20141113092555_permissions.rb
+++
@@ -1,8 +0,0 @@
-class Permissions < ActiveRecord::Migration
-  def change
-    create_table :permissions, :id => false do |t|
-      t.integer :user_id
-      t.integer :roles_id
-    end
-  end
-end
--- db/migrate/20141113101042_add_timestamps_to_permissions.rb
+++
@@ -1,8 +0,0 @@
-class AddTimestampsToPermissions < ActiveRecord::Migration
-  def change
-    add_column :permissions, :created_at, :datetime
-    add_column :permissions, :updated_at, :datetime
-    rename_column :permissions, :roles_id, :role_id
-    add_column :permissions, :id, :integer
-  end
-end
