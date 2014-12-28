c9b2c4f7c4a5f12e1d01d554370983e3562c85b1
ranjanin <nvranjani@gmail.com>
2014-12-28 02:13:54 +0000

    migrations

--- db/migrate/20141121091933_remove_columns_from_admin.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnsFromAdmin < ActiveRecord::Migration
-  def change
-    remove_column :admins, :name
-    remove_column :admins, :email
-    remove_column :admins, :phone_number
-    remove_column :admins, :address
-    remove_column :admins, :city
-    end
-end
--- db/migrate/20141121093822_add_authuser_id_to_admin.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthuserIdToAdmin < ActiveRecord::Migration
-  def change
-    add_column :admins, :authuser_id, :integer
-  end
-end
