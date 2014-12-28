034b7dd8542440d1b8dec2afab9c66f13c4b356a
ranjanin <nvranjani@gmail.com>
2014-12-28 02:24:59 +0000

    migrations

--- db/migrate/20141125104154_change_mcategory_id_name_in_user_caegories.rb
+++ db/migrate/20141125104154_change_mcategory_id_name_in_user_caegories.rb
@@ -1,5 +1,5 @@
 class ChangeMcategoryIdNameInUserCaegories < ActiveRecord::Migration
   def change
-    change_column :usercategories, :m_categoy_id, :m_category_id
+    rename_column :usercategories, :m_categoy_id, :m_category_id
   end
 end
