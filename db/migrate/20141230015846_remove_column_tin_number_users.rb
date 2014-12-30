cea69bcbff97b7583b0163aaebdc9195844b9560
ranjanin <nvranjani@gmail.com>
2014-12-30 02:34:59 +0000

    tin number type string

--- db/migrate/20141230015846_remove_column_tin_number_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnTinNumberUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :tin_number
-    add_column :users, :tin_number, :integer, :limit => 11
-  end
-end
