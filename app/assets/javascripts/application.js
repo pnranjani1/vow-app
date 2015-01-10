2491ac314c5e95fde304cdbf7909bef7f20b3f42
ranjanin21 <parameswari.natarajan3@gmail.com>
2015-01-10 07:45:42 +0000

    changed the order in application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
 //= require bootstrap.min
+//= require jquery
 //= require nested_form
 //= require camera
 //= require html5shiv
