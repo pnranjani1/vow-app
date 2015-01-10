a310b39a19a874e1e52a9780ede28654696d5adc
ranjanin21 <parameswari.natarajan3@gmail.com>
2015-01-10 11:27:58 +0000

    design modified

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,37 +13,20 @@
 
 
 //= require turbolinks
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
 //= require nested_form
+//= require_tree .
+//= require bootstrap.min
 //= require camera
-//= require html5shiv
 //= require jquery.carouFredSel-6.1.0-packed
 //= require jquery.easing.1.3
-//= require jquery.equalheights
-//= require jquery.mobile.customized.min
-//= require jquery.mobilemenu
-//= require jquery.touchSwipe.min
-//= require jquery-migrate-1.2.1.min
-//= require sForm
-//= require respond.min
-//= require touchTouch.jquery
-//= require area
-//= require calendar
-//= require dashboard
-//= require donut
-//= require fullcalendar.min
-//= require jquery.icheck
 //= require jquery.nicescroll.min
 //= require jquery-1.10.1.min
-//= require jquery-ui-1.9.2.custom.min
-//= require morris.min
-//= require raphael-2.1.2.min
+//= require jquery-migrate-1.2.1.min
 //= require select2
 //= require target-admin
-//= require_tree .
-
+//= require touchTouch.jquery
+//= require jquerycommen
 
 
