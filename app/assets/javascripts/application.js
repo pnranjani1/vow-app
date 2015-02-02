7847a575e4797147af5fed5d65e0cc5aeb705318
ranjanin21 <parameswari.natarajan3@gmail.com>
2015-02-02 11:10:34 +0000

    corrected tally import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,38 +45,7 @@ $(document).ready(function(){
   });
   
   
-/* For nested_form in new_bill  */
 
-  $(document).on('nested:fieldAdded:line_items', function(event){
-  var field = event.field;
-
-  $(field).on('change', function(){    
-    var quantity = field.find('input.quantity').val().trim();
-    var unit_price = field.find('input.unit-price').val().trim();
-    var final_result = getTotalRate(quantity, unit_price)
-    
-    var total_item_price_field = field.find('input.unit-price').parent().find('input.total-item-price'); 
-
-    if(final_result >= 0){
-     $(total_item_price_field).val(final_result);
-    }
-
-  });
-})
-
- 
-function getTotalRate(duration, task_rate) {
-  var quantity = parseInt(quantity);
-  var unit_price = parseInt(unit_price); 
-  var result = quantity * unit_price
-  
-  if(result >= 0){
-    return result; 
-  }  
-}
-  
-  
-  
 
  
 var App = function(){
