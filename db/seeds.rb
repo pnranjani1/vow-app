4c9bc2c49baa62cfccdf02f0a63cfc01c05123b9
ranjanin21 <parameswari.natarajan3@gmail.com>
2015-05-02 11:49:02 +0000

    corrections in new bill css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Authuser.delete_all
 
-Authuser.create! ([
-  {id: 1, name: "P Natarajan", email: "natarajan.perumalsamy@gmail.com", password: "abcdefgh", approved: true}
-  ])
 
-#Authuser.create(name: "P Natarajan", email: "natarajan.perumalsamy@gmail.com", password: "abcdefgh", approved: true)
-#Membership.create(authuser_id: 1, phone_number: "1234567890", membership_satrt_date: Date.today, membership_end_date: Date.today+1.year)
-#Address.create(authuser_id: 1, address_line_1: "PG Pudur, Pollachi", city: "CBE", state: "TN", country: "India")
-#Permission.create(authuser_id: 1, main_role_id:  1.to_i)
 
