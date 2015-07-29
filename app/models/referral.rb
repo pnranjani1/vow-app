5ae2fc76556eea1ccfb59f08212528a514b23711
ranjanin21 <parameswari.natarajan3@gmail.com>
2015-07-29 10:17:47 +0000

    validations added for referrals

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -5,5 +5,5 @@ class Referral < ActiveRecord::Base
   has_many :clients
   belongs_to :referral_type
   
-  
+  validates :name, :address_line_1, :address_line_2, :state, :country, :mobile_number, :email, :referral_type_id , presence: true
 end
