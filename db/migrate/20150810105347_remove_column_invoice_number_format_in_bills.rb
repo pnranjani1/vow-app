fb0606ce03867c2b7ca667ff6c3c49d08dbc5fc9
ranjanin <ranjani@iprimitus.com>
2015-08-11 08:09:29 +0000

    error corrected in migration file

--- db/migrate/20150810105347_remove_column_invoice_number_format_in_bills.rb
+++ db/migrate/20150810105347_remove_column_invoice_number_format_in_bills.rb
@@ -1,6 +1,7 @@
 class RemoveColumnInvoiceNumberFormatInBills < ActiveRecord::Migration
   def change
     remove_column :bills, :invoice_number_format
-    add_column :bills, :invoice_number_format, :boolean, :default => 1
+    add_column :bills, :invoice_number_format, :boolean
+    #, :default => 1
   end
 end
