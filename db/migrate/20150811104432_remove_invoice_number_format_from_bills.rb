250e75519a2246c10a2a3e96e6e85c6e282cf6a0
ranjanin <ranjani@iprimitus.com>
2015-08-12 13:15:49 +0000

    migration error corrected

--- db/migrate/20150811104432_remove_invoice_number_format_from_bills.rb
+++ db/migrate/20150811104432_remove_invoice_number_format_from_bills.rb
@@ -1,6 +1,6 @@
 class RemoveInvoiceNumberFormatFromBills < ActiveRecord::Migration
   def change
-    remove_column :bills, :invoice_number_formats
+    #remove_column :bills, :invoice_number_formats
     add_column :bills, :record_number, :string
   end
 end
