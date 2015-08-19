c2d8dce9ce2c39c9b94dfe4994e96b9175f1f04b
ranjanin <ranjani@iprimitus.com>
2015-08-19 08:07:06 +0000

    invoice number corrected

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -555,9 +555,11 @@ end
               number_updated = invoice.number.to_i + 1
               if number_updated.to_s.length < 6 
                 updated_number = number_updated.to_s.rjust(6, '0')
-                @number = updated_number.to_s + " " + user.invoice_string 
+                @number = updated_number.to_s 
               end
             else 
+              invoice = InvoiceRecord.where(:authuser_id => user.id).last
+              number_updated = invoice.number.to_i + 1
               @number = number_updated.to_s + " " + user.invoice_string
             end
           end #user invoice record ends
@@ -583,6 +585,8 @@ end
                  @number = updated_number.to_s + " " + primary_user.invoice_string 
                end
              else
+               invoice = InvoiceRecord.where(:authuser_id => user.id).last
+               number_updated = invoice.number.to_i + 1
                @number = number_updated.to_s + " " + primary_user.invoice_string
              end
            end # primary user invoice record ends
