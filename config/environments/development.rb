f978752bf817ea9bd4be581dea58c938afb7e20b
pnranjani <ranjani@iprimitus.com>
2015-10-10 08:29:26 +0000

    spelling mistake corrected

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = {:host => 'http://purple-wagon-47-179691.apse1.nitrousbox.com', port: 3000 }
+  config.action_mailer.default_url_options = {:host => 'http://ruby-on-rails-126001.nitrousapp.com', port: 3000 }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
